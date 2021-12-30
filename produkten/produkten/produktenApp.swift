7bdd018ab833c6e8b072b0741a5651c489389448
arachnothrone <arachnothrone@gmail.com>
2021-12-29 22:41:24 -0500

    Add ios proj

---
+++ produkten/produkten/produktenApp.swift
@@ -0,0 +1,17 @@
+//
+//  produktenApp.swift
+//  produkten
+//
+//  Created by arachnothrone on 2021-12-29.
+//
+
+import SwiftUI
+
+@main
+struct produktenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
