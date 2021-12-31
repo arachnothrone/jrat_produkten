048b7fc6f96a6658e31aada3b399ff37a4e25d9f
arachnothrone <arachnothrone@gmail.com>
2021-12-31 01:02:25 -0500

    Create all manually for tomorrow's grocery trip

--- produkten/produkten/ContentView.swift
+++ produkten/produkten/ContentView.swift
@@ -14,49 +14,89 @@ struct CostcoItem: Identifiable {
 }
 
 private var items = [
-    CostcoItem(name: "Moloko 4L", price: 8.99),
-    CostcoItem(name: "Bread MillerMills", price: 5.43),
-    CostcoItem(name: "Coffe Columbian", price: 12.34),
-    CostcoItem(name: "I_1", price: 0.1),
-    CostcoItem(name: "I_2", price: 0.1),
-    CostcoItem(name: "I_3", price: 0.1),
-    CostcoItem(name: "I_4", price: 0.1),
-    CostcoItem(name: "I_5", price: 0.1),
-    CostcoItem(name: "I_6", price: 0.1),
-    CostcoItem(name: "I_7", price: 0.1),
-    CostcoItem(name: "I_8", price: 0.1),
-    CostcoItem(name: "I_9", price: 0.1),
-    CostcoItem(name: "I_10", price: 0.1),
-    CostcoItem(name: "I_11", price: 0.1),
-    CostcoItem(name: "I_12", price: 0.1),
-    CostcoItem(name: "I_13", price: 0.1),
-    CostcoItem(name: "I_14", price: 0.1),
-    CostcoItem(name: "I_15", price: 0.1),
-    CostcoItem(name: "I_16", price: 0.1),
-    CostcoItem(name: "I_17", price: 0.1),
-    CostcoItem(name: "I_18", price: 0.1)
+    CostcoItem(name: "Moloko 4L", price: 8.29),
+    CostcoItem(name: "Bread MillerMills", price: 6.99),
+    CostcoItem(name: "Coffe Columbian", price: 11.49),
+    CostcoItem(name: "Cheese Mozzarella 1kg", price: 12.99),
+    CostcoItem(name: "Cheese Port Salut", price: 5.99),
+    CostcoItem(name: "Cheese Port Salut", price: 5.99),
+    CostcoItem(name: "Cheese PleineLune", price: 12.79),
+    CostcoItem(name: "Cheese Jarlsberg slices", price: 12.49),
+    CostcoItem(name: "Cheese Jarlsberg brick", price: 12.49),
+    CostcoItem(name: "Eggs 24", price: 7.59),
+    CostcoItem(name: "Sour cream", price: 4.49),
+    CostcoItem(name: "Cottage cheese", price: 9.99),
+    CostcoItem(name: "Cottage cheese", price: 9.99),
+    CostcoItem(name: "Ricotta", price: 12.00),
+    CostcoItem(name: "Butter", price: 4.49),
+    CostcoItem(name: "Youghurt YOP", price: 9.99),
+    CostcoItem(name: "Fish red", price: 40.0),
+    CostcoItem(name: "Salmon DOM smoked", price: 15.99),
+    CostcoItem(name: "Lean ground Beef", price: 25.0),
+    CostcoItem(name: "Frozen berries", price: 15.00),
+    CostcoItem(name: "Ice cream Moose", price: 6.49),
+    CostcoItem(name: "Ice cream Moose vanilla", price: 6.49),
+    CostcoItem(name: "Ice cream mochi", price: 14.99),
+    CostcoItem(name: "Tuna cans (6x)", price: 10.00),
+    CostcoItem(name: "Pickes", price: 10.00),
+    CostcoItem(name: "Whipped cream", price: 8.49),
+    CostcoItem(name: "Coffee Lavazza", price: 15.00),
+    CostcoItem(name: "Pepperkaka (gingerbread)", price: 5.69),
+    CostcoItem(name: "Oranges", price: 11.99),
+    CostcoItem(name: "Mandarin", price: 5.59),
+    CostcoItem(name: "Beetroot (svekla)", price: 10.00),
+    CostcoItem(name: "Cucumbers", price: 6.49),
+    CostcoItem(name: "Cucumbers", price: 6.49),
+    CostcoItem(name: "Tomatoes", price: 8.99),
+    CostcoItem(name: "Potato", price: 5.99),
+    CostcoItem(name: "Onion shallot", price: 10.00),
+    CostcoItem(name: "Shampoo", price: 8.99),
+    CostcoItem(name: "Cat litter toilet", price: 9.99),
+    CostcoItem(name: "Antistatic napkins for laundry", price: 15.00),
+    CostcoItem(name: "-", price: 5.00),
+    CostcoItem(name: "Salsa gut", price: 8.49),
+    CostcoItem(name: "Que Pasa", price: 5.99),
+    CostcoItem(name: "-", price: 10.00),
+    CostcoItem(name: "-", price: 15.00),
+    CostcoItem(name: "-", price: 20.00),
 ]
 
 struct ContentView: View {
 
-
     @State private var multiSelection = Set<UUID>()
+    @State var value: Double = 0.0
+    
     var body: some View {
 
         NavigationView {
             List(items, selection: $multiSelection) {
                 Text($0.name + ": $" + String($0.price))
             }
-            .navigationTitle("Goods to find")
+            .navigationTitle("Costco list")
             .toolbar {EditButton()}
             
         }
         
         Text("\(multiSelection.count) selections")
-//        { print("Items: \(multiSelection)")
-//        }
         
-        Button("ion") {for a in multiSelection {print("---> \(a.uuidString)")}}
+//        Button("ion") {for a in multiSelection {print("---> \(a.uuidString)")}}
+        Button("Selected value:") {
+            var summa = 0.0
+            for z in items {
+                for s in multiSelection {
+                    if ("\(z.id)" == s.uuidString) {
+                        print("\(z.name) -> \(z.price)")
+                        summa += z.price
+                    }
+                }
+                //contains(entities, { $0 is Player } )
+            }
+            value = summa
+        }
+        Text(String(format: "$ %3.2f", value))
+//            .onChange(of: value, perform: { value in
+//                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+//            })
         
     }
 }
